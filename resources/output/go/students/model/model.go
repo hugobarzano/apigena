c1063ca559dc691f14ccbddd91ce2f77090e1ad6
CESAR HUGO BARZANO CRUZ <cesarhugo.barzano.next@bbva.com>
2020-04-10 18:16:53 +0200

    re-generate studens examplee2

---
+++ resources/output/go/students/model/model.go
@@ -0,0 +1,16 @@
+package model
+
+type STUDENTS struct {
+	Address STUDENTS_sub1 `json:"address" yml:"address"`
+	Age     int64         `json:"age" yml:"age"`
+	Car     bool          `json:"car" yml:"car"`
+	Hobbies []string      `json:"hobbies" yml:"hobbies"`
+	Name    string        `json:"name" yml:"name"`
+    ID string      `json:"id" yml:"id"` 
+}
+
+type STUDENTS_sub1 struct {
+	City   string `json:"city" yml:"city"`
+	Number int64  `json:"number" yml:"number"`
+	Street string `json:"street" yml:"street"`
+}
